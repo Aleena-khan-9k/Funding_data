f280e28380264232636ce80f1753347939aef6f1
Aleena-khan-9k <aleena@bizprospex.com>
2024-12-16 11:03:44 +0530

    last

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,16 @@
+import React from 'react';
+import './App.css';
+import Data from './components/main.js'; // Import the left side component (upload data)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-        
-        <a
-          className="App-link"
-
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
+      <div className="left-side">
+        <Data />  {/* Left side with upload data */}
+      </div>
+      <div className="right-side">
+        {/* Right side content will be added later */}
+      </div>
     </div>
   );
 }
